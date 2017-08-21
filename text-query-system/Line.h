7d86eaa1b747594dfd33a3e3eaea6758c93f5ad0
Unknown <95ry95ry@gmail.com>
2017-08-21 17:27:40 +0200

    Exercise 5.2

--- text-query-system/Line.h
+++ text-query-system/Line.h
@@ -3,21 +3,22 @@
 
 #include "Word.h"
 #include <string>
+#include <vector>
+
 using namespace std;
 
 class Line
 {
 public:
-	// DO NOT MODIFY THE PUBLIC INTERFACE OF THIS CLASS
+    // DO NOT MODIFY THE PUBLIC INTERFACE OF THIS CLASS
 
-	// accepts a line of text (which FileReader will provide in production code)	
-	Line(const string& line);
-	// returns true if the search word is found in the line; false otherwise
-	bool contains(const Word& search_word) const;
+    // accepts a line of text (which FileReader will provide in production code)
+    Line(const string& line);
+    // returns true if the search word is found in the line; false otherwise
+    bool contains(const Word& search_word) const;
 
-private:		
-
+private:
+    vector<Word> _line;
 };
 
 #endif
-
